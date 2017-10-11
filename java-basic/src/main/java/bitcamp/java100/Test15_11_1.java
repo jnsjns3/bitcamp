5e0fdebd3d43751badab113d7f2e92dbb386649e
jnsjns3 <jnsjns3@naver.com>
2017-10-11 11:39:39 +0900

    .

--- java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
+++ java-basic/src/main/java/bitcamp/java100/Test15_11_1.java
@@ -19,7 +19,7 @@ public class Test15_11_1{
            
            boolean otherRead;
            boolean otherWrite;
-           boolean otherExe;
+           boolean otherExe; 
           
            
            
